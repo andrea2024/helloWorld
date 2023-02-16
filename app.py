682d74db67e218aecc00bb57793d3c7277152286
andrea2024 <aespino1@terpmail.umd.edu>
2023-02-15 21:12:11 -0500

    Added the about me page to practice HTML in class.

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Andrea! I am adding my first code change'
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
